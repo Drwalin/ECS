91076f73ccbb4aac084f881cc715d087a9053ef2
Drwalin <34726800+Drwalin@user.noreply.github.com>
2026-09-18 18:16:42 +0200

    Declared more interface.

---
+++ include/ecs/ecs_header_macros.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#define ECS_TYPE_TRAITS_HEADER(TYPE_NAME, SHORT_NAME_OR_INT, POCO, ALIGNEMENT) \
+	static const ecs::TypeTraits *_ecs_get_type_traits();                      \
+                                                                               \
+	constexpr static inline const char *_ecs_type_name = #TYPE_NAME;           \
+	constexpr static inline std::string _ecs_name = _ecs_type_name;            \
+	constexpr static inline StringOrInt _ecs_short_type_name_or_id =           \
+		SHORT_NAME_OR_INT;                                                     \
+	constexpr static inline uint32_t _ecs_custom_alignement = ALIGNEMENT;      \
+	constexpr static inline bool _ecs_poco = POCO;                             \
+	static ecs::ComponentId _ecs_component_id;                                 \
+                                                                               \
+	static void _ecs_assure(ecs::Entity, ecs::ComponentManager *);             \
+	static TYPE_NAME &_ecs_access(ecs::Entity, ecs::ComponentManager *);       \
+	static TYPE_NAME &_ecs_add(ecs::Entity, ecs::ComponentManager *);          \
+	static TYPE_NAME &_ecs_set(ecs::Entity, ecs::ComponentManager *,           \
+							   const TYPE_NAME &val);                          \
+	static TYPE_NAME &_ecs_set(ecs::Entity, ecs::ComponentManager *,           \
+							   TYPE_NAME &&mov_val);                           \
+	static TYPE_NAME *_ecs_get(ecs::Entity, const ecs::ComponentManager *);    \
+	static void _ecs_remove(ecs::Entity, ecs::ComponentManager *);             \
+                                                                               \
+	static void _ecs_execute_observers_add(ecs::Entity,                        \
+										   ecs::ComponentManager *);           \
+	static void _ecs_execute_observers_set(ecs::Entity,                        \
+										   ecs::ComponentManager *);           \
+	static void _ecs_execute_observers_remove(ecs::Entity,                     \
+											  ecs::ComponentManager *);        \
+                                                                               \
+	static void _ecs_move_to_empty(TYPE_NAME &&src, TYPE_NAME &dst);           \
+	static void _ecs_copy_to_empty(const TYPE_NAME &src, TYPE_NAME &dst);      \
+	static void _ecs_move_to_existing(TYPE_NAME &&src, TYPE_NAME &dst);        \
+	static void _ecs_copy_to_existing(const TYPE_NAME &src, TYPE_NAME *dst);   \
+	static void _ecs_construct(TYPE_NAME *ptr);                                \
+	static void _ecs_destruct(TYPE_NAME *ptr);                                 \
+                                                                               \
+	static void _ecs_serialize(struct serializer &ar, const TYPE_NAME &ptr);   \
+	static void _ecs_deserialize(struct deserializer &ar, TYPE_NAME &ptr);     \
+                                                                               \
+	void serialize(struct serializer &ar) const;                               \
+	void deserialize(struct deserializer &ar);
+
+#define ECS_TAG_TRAITS(TYPE_NAME, SHORT_NAME_OR_INT)                           \
+	static const ecs::TagTraits *_ecs_get_tag_traits();                        \
+                                                                               \
+	constexpr static inline const char *_ecs_type_name = #TYPE_NAME;           \
+	constexpr static inline std::string _ecs_name = _ecs_type_name;            \
+	constexpr static inline StringOrInt _ecs_short_type_name_or_id =           \
+		SHORT_NAME_OR_INT;                                                     \
+	static ecs::TagId _ecs_tag_id;
---
+++ include/ecs/ecs_types.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <cstdint>
+
+#include <string>
+
+struct StringOrInt {
+	std::string s;
+	uint32_t i;
+};
+
+namespace ecs
+{
+class World;
+class ComponentManager;
+class TagManager;
+
+struct Entity {
+	uint32_t id;
+	uint32_t version;
+};
+
+struct ComponentId {
+	uint32_t id;
+};
+
+struct TagId {
+	uint32_t id;
+};
+
+struct TypeTraits {
+	void (*const assure)(Entity, ComponentManager *);
+	void *(*const access)(Entity, ComponentManager *);
+	void *(*const add)(Entity, ComponentManager *);
+	void *(*const set_copy)(Entity, ComponentManager *, const void *ptr);
+	void *(*const set_move)(Entity, ComponentManager *, void *mov_ptr);
+	void *(*const get)(Entity, const ComponentManager *);
+	void (*const remove)(Entity, ComponentManager *);
+
+	void (*const execute_observers_add)(Entity, ComponentManager *);
+	void (*const execute_observers_set)(Entity, ComponentManager *);
+	void (*const execute_observers_remove)(Entity, ComponentManager *);
+
+	void (*const move_to_empty)(void *src, void *dst);
+	void (*const copy_to_empty)(const void *src, void *dst);
+	void (*const move_to_existing)(void *src, void *dst);
+	void (*const copy_to_existing)(const void *src, void *dst);
+	void (*const construct)(void *ptr);
+	void (*const destruct)(void *ptr);
+
+	void (*const serialize)(struct serializer &ar, const void *ptr);
+	// deserialize into existing
+	void (*const deserialize)(struct deserializer &ar, void *ptr);
+
+	const char *type_name;
+	const std::string name;
+	const StringOrInt short_type_name_id; // used for serialzation
+	const uint32_t bytes;
+	const uint32_t alignement;
+	const bool poco;
+	const ComponentId component_id;
+};
+
+struct TagTypeTraits {
+	const char *type_name;
+	const std::string name;
+	const TagId tag_id;
+};
+} // namespace ecs
